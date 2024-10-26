40df0d61b818edfce424163dbd10c66e6ab195ee
Ayodele Olufemi <tekxpo@gmail.com>
2024-10-26 18:45:26 +0100

    resolved index.js

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-<<<<<<< HEAD
-const messages = [ ];
-=======
-const messages = "Waiting for messages !";
-const sentMessages = ['empty array'];
->>>>>>> 39b6ecb0daa9744ae5541a88690ca97dd11a93fc
+const messages = [];
